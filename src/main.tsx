cbcff6e8714571d50b7c7973b5910f27cc0b825c
Zahliada VM <waah777@gmail.com>
2024-12-13 15:21:25 +0200

    форматирование плагином "prettier"

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./components/app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
