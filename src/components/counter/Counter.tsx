9beaee6a651c52ee6000cddf8e44cee944b43196
Zahliada VM <waah777@gmail.com>
2024-12-16 12:21:43 +0200

    перенёс счётчик в отдельный компонент

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import "./Counter.scss";
+
+export const Counter = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="counter">
+			<button onClick={() => setCount((count) => count + 1)}>
+				count is {count}
+			</button>
+			<p>
+				Edit <code>src/components/counter/Counter.tsx</code> and save to test
+				HMR
+			</p>
+		</div>
+	);
+};
