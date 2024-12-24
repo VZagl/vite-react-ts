e38bad42bdda9f7d4bd74a7c30d1d9f9ef3cd85f
Zahliada VM <waah777@gmail.com>
2024-12-24 16:57:55 +0200

    добавил плагин сортировки импортов в JSX.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,18 @@
 module.exports = {
 	// 0 = off, 1 = warn, 2 = error
-	trailingComma: 'es5',
-	singleQuote: true,
-	jsxSingleQuote: true,
-	arrowParens: 'always',
+	// trailingComma: 'es5',
+	// singleQuote: false,
+	// jsxSingleQuote: true,
+	// arrowParens: 'always',
 	maxLen: ['warn', 140, 2],
 	tabWidth: 2,
 	useTabs: true,
+	// semi: true,
 	// linebreakStyle: ['off', 'windows'],
-	plugins: ['prettier-plugin-organize-attributes', 'prettier-plugin-css-order'],
+	plugins: [
+		'prettier-plugin-organize-imports',
+		'prettier-plugin-organize-attributes',
+		'prettier-plugin-css-order',
+	],
+	// htmlWhitespaceSensitivity: 'css',
 };
