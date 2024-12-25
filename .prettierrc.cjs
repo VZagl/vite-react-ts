b934751624daf0c3a1dc4584b9c822ee404fe8d2
Zahliada VM <waah777@gmail.com>
2024-12-25 08:35:20 +0200

    изменил настройки Prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,13 +1,17 @@
 module.exports = {
 	// 0 = off, 1 = warn, 2 = error
-	// trailingComma: 'es5',
-	// singleQuote: false,
-	// jsxSingleQuote: true,
-	// arrowParens: 'always',
-	maxLen: ['warn', 140, 2],
 	tabWidth: 2,
 	useTabs: true,
-	// semi: true,
+	quoteProps: 'as-needed',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+	maxLen: ['warn', 140, 2],
+	semi: true,
+	embeddedLanguageFormatting: 'auto',
 	// linebreakStyle: ['off', 'windows'],
 	plugins: [
 		'prettier-plugin-organize-imports',
@@ -15,4 +19,6 @@ module.exports = {
 		'prettier-plugin-css-order',
 	],
 	// htmlWhitespaceSensitivity: 'css',
+	// attributeGroups: ['^class$', '^(id|name)$', '$DEFAULT', '^aria-'],
+	// attributeSort: 'ASC',
 };
